1190aa1cc74ff5969866271a9a7c784a4da14071
pavithra938163 <pavithra938163@gmail.com>
2026-03-08 19:52:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const schoolRoutes = require("./routes/schoolRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api", schoolRoutes);
+
+app.get("/", (req, res) => {
+  res.send("School Management API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
